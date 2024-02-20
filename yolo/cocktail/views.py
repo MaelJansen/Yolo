1d5db533531ebef0b06fb1293d255f3fe95648a9
MaelJansen <maeljansen2003@gmail.com>
2024-02-20 14:35:46 +0100

    Ajout de la liste des cocktails

--- yolo/cocktail/views.py
+++ yolo/cocktail/views.py
@@ -4,5 +4,6 @@ from .models import Cocktail
 def liste_cocktail(request):
     Cocktail.fetch_and_create()
     cocktail = Cocktail.objects.all()
+    
     return render(request, 'liste_cocktail.html', {'cocktails': cocktail})
 
