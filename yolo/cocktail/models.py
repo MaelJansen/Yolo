4f4ff2ee6d18602119c02d412cd26713a4f15906
juledupont <jules.dupont.1@etu.u-bordeaux.fr>
2024-02-21 11:39:10 +0100

    cocktails now have ingredients

--- yolo/cocktail/models.py
+++ yolo/cocktail/models.py
@@ -3,28 +3,31 @@ from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+
 class Cocktail(models.Model):
-     name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
-     panels = [
-          FieldPanel("name")
-     ]
+    panels = [
+        FieldPanel("name")
+    ]
 
-     def __str__(self) -> str:
-          return self.name
-     
+    def __str__(self) -> str:
+        return self.name
+
 
 class CocktailPage(Page):
     strDrink = models.CharField(max_length=200)
     strDrinkThumb = models.CharField(max_length=255)
     idDrink = models.CharField(max_length=200)
+    ingredients = models.JSONField(default=list)
 
     content_panels = Page.content_panels + [
         FieldPanel("strDrink"),
         FieldPanel("strDrinkThumb"),
         FieldPanel("idDrink"),
+        FieldPanel("ingredients")
     ]
-    
+
     search_auto_update = False
 
 
@@ -33,21 +36,20 @@ class CocktailIndexPage(Page):
         return (
             CocktailPage.objects.live()
         )
-    
+
     def paginate(self, request, *args):
-            page = request.GET.get("page")
-            paginator = Paginator(self.get_cocktails(), 100)
-            try:
-                pages = paginator.page(page)
-            except PageNotAnInteger:
-                pages = paginator.page(1)
-            except EmptyPage:
-                pages = paginator.page(paginator.num_pages)
-            return pages
-    
+        page = request.GET.get("page")
+        paginator = Paginator(self.get_cocktails(), 100)
+        try:
+            pages = paginator.page(page)
+        except PageNotAnInteger:
+            pages = paginator.page(1)
+        except EmptyPage:
+            pages = paginator.page(paginator.num_pages)
+        return pages
+
     def get_context(self, request):
         context = super().get_context(request)
-        cocktails =  self.get_cocktails()#self.paginate(request, self.get_cocktails())
+        cocktails = self.get_cocktails()  # self.paginate(request, self.get_cocktails())
         context["Cocktails"] = Cocktail.objects.create(name="Margarita")
         return context
-    
