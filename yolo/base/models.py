f9a9dcb6397c7a1462906ddfb2e4820069ae8ce7
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2024-02-22 11:01:41 +0100

    models

--- yolo/base/models.py
+++ yolo/base/models.py
@@ -1,3 +1,60 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from wagtail.admin.panels import (
+    FieldPanel,
+
+)
+
+from wagtail.fields import StreamField
+from wagtail.models import (
+    Collection,
+
+    Page,
+
+)
+from wagtail.search import index
+
+from .blocks import BaseStreamBlock
+
+
+class GalleryPage(Page):
+    """
+    This is a page to list locations from the selected Collection. We use a Q
+    object to list any Collection created (/admin/collections/) even if they
+    contain no items. In this demo we use it for a GalleryPage,
+    and is intended to show the extensibility of this aspect of Wagtail
+    """
+
+    introduction = models.TextField(help_text="Text to describe the page", blank=True)
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="Landscape mode only; horizontal width between 1000px and " "3000px.",
+    )
+    body = StreamField(
+        BaseStreamBlock(), verbose_name="Page body", blank=True, use_json_field=True
+    )
+    collection = models.ForeignKey(
+        Collection,
+        limit_choices_to=~models.Q(name__in=["Root"]),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        help_text="Select the image collection for this gallery.",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("introduction"),
+        FieldPanel("body"),
+        FieldPanel("image"),
+        FieldPanel("collection"),
+    ]
+
+    # Defining what content type can sit under the parent. Since it's a blank
+    # array no subpage can be added
+    subpage_types = []
+
